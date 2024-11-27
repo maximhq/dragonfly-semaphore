5a3fda4d0e97588019a270a5ba2847de9f18706c
KanishkaHalder1771 <kanishkahalder@gmail.com>
2024-11-27 17:40:01 +0530

    Updated connection name fetching

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,22 @@
+import { ClusterOptions, RedisOptions } from 'ioredis'
 import { RedisClient } from '../types'
 import createEval from './createEval'
 
 export { createEval }
+
+function isRedisOptions(
+  options: RedisOptions | ClusterOptions | undefined
+): options is RedisOptions {
+  return !!options && 'connectionName' in options
+}
 
 export async function delay(ms: number) {
   return await new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export function getConnectionName(client: RedisClient) {
-  const connectionName = client.options?.connectionName
+  const connectionName = isRedisOptions(client.options)
+    ? client.options?.connectionName
+    : client.options?.redisOptions?.connectionName
   return connectionName ? `<${connectionName}>` : '<unknown client>'
 }
